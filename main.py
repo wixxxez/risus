114ea5fd96f579db1d4dd267c5cdc0e9ecbb3c2c
artem.herasymiuk <vinewood02@gmail.com>
2021-12-31 16:24:12 +0200

    create sticker reply method

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ bot = aiogram.Bot(config.TOKEN);
 
 eventHandler = aiogram.Dispatcher(bot);
 
-@eventHandler.message_handler()
-async def echo( message ):
-    await message.answer(message["from"].first_name);
+@eventHandler.message_handler(content_types=['sticker','text'])
+async def ds( message ):
+    id = message.chat.id;
+
+
+    await bot.send_sticker(chat_id=id, sticker='CAACAgIAAxkBAAN6Yc8PDm4Vq6MQasoMsTesgaFoWMsAAncBAAIsYPUcWWKFR1r1NF0jBA',reply_to_message_id=message.message_id);
 
 if __name__ == "__main__":
     aiogram.executor.start_polling(eventHandler,skip_updates=True);
