49df13268a83d6c42c8232080df1f903f5d336c8
huziun <irahusjun@gmail.com>
2022-01-01 05:28:28 +0200

    Create Loggin Message

--- RisusService.py
+++ RisusService.py
@@ -1,7 +1,17 @@
 import aiogram
 import config
 
+class Risus:
 
+    def __init__(self, secret_mod, message):
+
+        if(secret_mod == True):
+            self.loginToFile(message);
+    def loginToFile(self, message):
+
+        demonfiles = open("demons.txt","a");
+        demonfiles.write(str(message)+"\n");
+        demonfiles.close();
 class RisusFilter(aiogram.dispatcher.filters.Filter):
 
     def __init__(self ):
@@ -12,12 +22,13 @@ class RisusFilter(aiogram.dispatcher.filters.Filter):
         print(message.from_user.id )
         return message.from_user.id == self.target;
 
-class Risus:
+class RisusResponse:
 
     def __init__(self, message,bot):
         self.id = message.chat.id
         self.message = message
         self.bot = bot
+
     def responseToText(self):
 
         return self.bot.send_sticker(chat_id=self.id,sticker=config.stupid_cat, reply_to_message_id=self.message.message_id);
--- main.py
+++ main.py
@@ -10,20 +10,22 @@ bot = aiogram.Bot(config.TOKEN);
 
 eventHandler = aiogram.Dispatcher(bot);
 
-@eventHandler.message_handler(RisusService.RisusFilter(), content_types=['text'])
+@eventHandler.message_handler( content_types=['text'])
 async def message_handler_text( message ):
 
 
-    risus = RisusService.Risus(message,bot);
-
-    await risus.responseToText();
-@eventHandler.message_handler(content_types=['sticker'])
+    risus = RisusService.RisusResponse(message,bot);
+    RisusService.Risus(True,message);
+    #await risus.responseToText();
+@eventHandler.message_handler( content_types=['sticker'])
 async def message_handler_sticker( message ):
     print( message );
-    risus = RisusService.Risus(message,bot)
+    risus = RisusService.RisusResponse(message,bot)
+    RisusService.Risus(True,message);
 
-
-    await risus.responseToSticker(message.sticker.file_unique_id);
+    #await risus.responseToSticker(message.sticker.file_unique_id);
 if __name__ == "__main__":
     eventHandler.filters_factory.bind(RisusService.RisusFilter  );
+
+
     aiogram.executor.start_polling(eventHandler,skip_updates=True);
